d08000587e9c77347826426314740c0d1fe603f5
Michael Kimball <mdk20@students.uwf.edu>
2016-04-02 04:12:45 -0500

    Aaaaaahhhhh push it

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 var express = require('express');
-var app = require('express')();
-var server = require('http').Server(app);
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var session = require('express-session');
+var passport = require('passport');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var app = express();
 
-server.listen(8080);
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+mongoose.connect(config.database);
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.listen(port);
 console.log("Server running at 8080");
